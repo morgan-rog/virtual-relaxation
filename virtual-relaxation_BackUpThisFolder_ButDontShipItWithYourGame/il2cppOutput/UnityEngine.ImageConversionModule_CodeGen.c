8cf498361b805a65f6f763c1e98b4b6c8eeaf8af
Morgan Rogers <morgan.rog@yahoo.com>
2022-08-28 17:53:38 -0500

    rest of stuff

---
+++ virtual-relaxation_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/UnityEngine.ImageConversionModule_CodeGen.c
@@ -0,0 +1,45 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Byte[] UnityEngine.ImageConversion::EncodeToEXR(UnityEngine.Texture2D,UnityEngine.Texture2D/EXRFlags)
+extern void ImageConversion_EncodeToEXR_m56D716F2C64F0BFC69A81D1787EB9D3E42A2EABA (void);
+static Il2CppMethodPointer s_methodPointers[1] = 
+{
+	ImageConversion_EncodeToEXR_m56D716F2C64F0BFC69A81D1787EB9D3E42A2EABA,
+};
+static const int32_t s_InvokerIndices[1] = 
+{
+	12002,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_UnityEngine_ImageConversionModule_CodeGenModule;
+const Il2CppCodeGenModule g_UnityEngine_ImageConversionModule_CodeGenModule = 
+{
+	"UnityEngine.ImageConversionModule.dll",
+	1,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
